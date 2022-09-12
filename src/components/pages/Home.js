b35b9df42ef7968e23aa876b5749593520fb7ab0
“João <eng.joao.pedro@outlook.com>
2022-09-12 19:55:27 -0300

    refactor: creating home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
+import LinkButton from "../layout/LinkButton";
+import saving from "../../img/savings.svg";
+import styles from "./Home.module.css";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar seu projeto agora mesmo!</p>
+      <LinkButton to="/" text="Criar Projeto"></LinkButton>
+      <img src={saving} alt="Costs"></img>
+    </section>
+  );
 }
 
 export default Home;
