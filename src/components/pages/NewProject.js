6042d2bd3e02a8830de0c8b7a6b3dc1b1ef629ff
“João <eng.joao.pedro@outlook.com>
2022-09-14 22:08:02 -0300

    refactor: fixing error className

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,12 @@
-import ProjectForm from "../form/ProjectForm";
+import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 function NewProject() {
   return (
-    <div classNAme={styles.newproject_container}>
+    <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm />
+      <ProjectForm btnText="Criar Projeto" />
     </div>
   );
 }
