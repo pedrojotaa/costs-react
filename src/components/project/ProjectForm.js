b41d31d4fdbff4680355e3f775eea8d84701abd6
“João <eng.joao.pedro@outlook.com>
2022-09-15 20:51:20 -0300

    refactor: connect API to the project

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => setCategories(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form>
       <Input
@@ -17,7 +33,11 @@ function ProjectForm({ btnText }) {
         text="Orçamento do projeto"
         name="budget"
       />
-      <Select name="caegory_id" text="Selecione a categoria" />
+      <Select
+        name="caegory_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
