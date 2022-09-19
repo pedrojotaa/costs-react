08c12e4a9e9a252eb9b907d289f62ffd7c89c4ce
“João <eng.joao.pedro@outlook.com>
2022-09-18 21:36:37 -0300

    refactor: showing projects  the page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,17 @@
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 import Message from "../layout/Message";
 
+import ProjectCard from "../project/ProjectCard";
+
+import styles from "./Projects.module.css";
+
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
 
   let message = "";
@@ -11,10 +20,38 @@ function Projects() {
     message = location.state.message;
   }
 
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h1>Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
       {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          //o retorno é um objeto, portanto é recebido 'entre parênteses ()'
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
+      </Container>
     </div>
   );
 }
